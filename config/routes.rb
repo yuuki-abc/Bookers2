197457bab38143a56d2860411621a08509d73d7d
yuuki-abc <yuki.abc.300@gmail.com>
2021-02-03 14:33:41 +0000

    [Update] toppageとリンクの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root 'books#top'
   devise_for :users
   # yukikome => deviseを使用する際にURLとしてusersを含むことを示している
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+
 end
