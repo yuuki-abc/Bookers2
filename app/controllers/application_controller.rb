9e9ce6a261aad3e6ca74c72fbc909de818d787f8
yuuki-abc <yuki.abc.300@gmail.com>
2021-02-04 14:45:49 +0000

    [Update] ログインしていないとtopページへリダイレクトするよう設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@
 
 class ApplicationController < ActionController::Base
 
+  before_action :authenticate_user!,except: [:top]
+  # yukikome => before_actionメソッドは、
+  # このコントローラが動作する前に実行される
+  # :authenticate_user!はログイン認証されていなければ、
+  # ログイン画面へリダイレクトするといった機能
+  # except: [:top]はトップ以外で、という意味
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   # yukikome => デバイス機能を利用する前に、メソッドconfigure_permitted_parametersを
